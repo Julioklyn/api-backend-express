8e1eb30807a2adadb744a679ee1c2fdc8fd85a43
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-08-12 17:47:12 -0300

    aprendizado de construcao de crud

--- src/server.js
+++ src/server.js
@@ -3,9 +3,30 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.put('/produto', (req, res) => {
+    //pegar dados do Usuário
+  res.json('Usuário editado com sucesso!')
 })
+
+app.get('/profile', (req, res) => {
+  res.json({ message: 'Obtendo dados do perfil' });
+});
+
+app.post('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados);
+  res.json({ message: 'Perfil criado com sucesso' });
+});
+
+app.put('/profile', (req, res) => {
+  res.json({ message: 'Perfil atualizado com sucesso' });
+});
+
+app.delete('/profile', (req, res) => {
+  res.json({ message: 'Perfil excluído com sucesso' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
