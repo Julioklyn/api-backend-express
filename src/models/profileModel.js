f1b8318db52690f7e3999cfe760e1f27cfe13d43
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-11-03 15:56:25 -0300

    refatoração para salvar hash seguro da senha do user no banco de dados

--- src/models/profileModel.js
+++ src/models/profileModel.js
@@ -1,32 +1,63 @@
-import {PrismaClient} from '../generated/prisma/index.js';
+import { PrismaClient } from '../generated/prisma/index.js'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export const create = async(profile) => {
+export const create = async (profile) => { 
     return await prisma.user.create({
-        data: profile
-    });
+        data: profile,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
-export const update = async(id, profile) => {
+export const update = async (id, profile) => {
     return await prisma.user.update({
-        where: {id},
-        data: profile
-    });
+        data: profile,
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }  
+    })
 }
 
 export const list = async () => {
-    return await prisma.user.findMany();
+    return await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
-export const getById = async(id) => {
+export const getById = async (id) => {
     return await prisma.user.findUnique({
-        where: {id}
-    });
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
-export const remove = async(id) => {
+export const remove = async (id) => {
     return await prisma.user.delete({
-        where: {id}
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
     })
 }
